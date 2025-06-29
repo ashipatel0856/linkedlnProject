9af3ac4921ff13bfefb587d76cf17a9da905b9c2
Ashish Kumar <ashishkumarr0856@gmail.com>
2025-06-30 02:40:24 +0530

    changes

--- ConnectionsService/src/main/java/com/ashish/linkedlnProject/ConnectionsService/repository/PersonRepository.java
+++ ConnectionsService/src/main/java/com/ashish/linkedlnProject/ConnectionsService/repository/PersonRepository.java
@@ -12,8 +12,17 @@ public interface PersonRepository extends Neo4jRepository<Person, String> {
     Optional<Person> findByUserId(Long userId);
 
     @Query("match (personA:Person) -[:CONNECTED_TO]- (personB:Person)"+
-            "where personA.userId = 2"+
+            "where personA.userId = 2" +
             "return personB")
     List<Person> getFirstDegreeConnectionsOfUser(Long userId);
 
+    boolean connectionRequestExits(Long sendId, Long receiverId);
+
+    boolean alreadyConnected(Long senderId, Long receiverId);
+
+    void addConnectionRequest(Long senderId, Long receiverId);
+
+    void acceptConnectionRequest(Long senderId, Long receiverId);
+
+    void rejectConnectionRequest(Long senderId, Long receiverId);
 }
--- ConnectionsService/src/main/java/com/ashish/linkedlnProject/ConnectionsService/service/ConnectionsService.java
+++ ConnectionsService/src/main/java/com/ashish/linkedlnProject/ConnectionsService/service/ConnectionsService.java
@@ -23,4 +23,65 @@ public class ConnectionsService {
         return personRepository.getFirstDegreeConnectionsOfUser(userId);
 
     }
+
+    public void sendConnectionRequest(Long receiverId){
+        Long senderId = AuthContextHolder.getCurrentUser();
+        log.info("sending connection request to sender with id:{}",senderId,receiverId);
+
+
+        if(senderId.equals(receiverId)){
+            throw new RuntimeException("both sender and receiver are equal");
+        }
+        boolean alreadySentRequest = personRepository.connectionRequestExits(senderId,receiverId);
+        if(alreadySentRequest){
+            throw new RuntimeException("connection request already exits, can not send again");
+        }
+
+        boolean alreadyConnected= personRepository.alreadyConnected(senderId,receiverId);
+        if(alreadyConnected){
+            throw new RuntimeException("already connected users, can not add connection request");
+        }
+        log.info("successfully sent the connection request to sender and receiver with id:{}",senderId);
+        personRepository.addConnectionRequest(senderId, receiverId);
+    }
+
+
+    public void acceptConnectionRequest(Long senderId){
+        Long receiverId = AuthContextHolder.getCurrentUserId();
+        log.info("accepting connection request to sender  with id:{}",senderId,receiverId);
+
+
+        if(senderId.equals(receiverId)){
+            throw new RuntimeException("both sender and receiver are equal");
+        }
+        boolean alreadyConnected= personRepository.alreadyConnected(senderId,receiverId);
+        if(alreadyConnected){
+            throw new RuntimeException("already connected users, can not accept connection request again");
+        }
+        boolean alreadySentRequest = personRepository.connectionRequestExits(senderId,receiverId);
+        if(alreadySentRequest){
+            throw new RuntimeException("no connection request already exits, can not accept without request");
+        }
+
+        personRepository.acceptConnectionRequest(senderId,receiverId);
+        log.info("successfully accepted connection request to sender and receiver with id:{}",senderId);
+    }
+
+
+    public void rejectConnectionRequest(Long senderId) {
+        Long receiverId = AuthContextHolder.getCurrentUserId();
+        log.info("rejecting a connection request to sender  with id:{}", senderId, receiverId);
+
+
+        if (senderId.equals(receiverId)) {
+            throw new RuntimeException("both sender and receiver are equal");
+        }
+        boolean alreadySentRequest = personRepository.connectionRequestExits(senderId, receiverId);
+        if (alreadySentRequest) {
+            throw new RuntimeException("no connection request  exits, can not reject it");
+        }
+
+        personRepository.rejectConnectionRequest(senderId, receiverId);
+        log.info("successfully reject connection request to sender and receiver with id:{}", senderId);
+    }
 }
