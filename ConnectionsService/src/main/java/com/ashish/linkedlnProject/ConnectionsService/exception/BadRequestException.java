7d66eef9717384c4598ab534aee398713e3bcc3b
Ashish Kumar <ashishkumarr0856@gmail.com>
2025-07-01 08:05:11 +0530

    changes

---
+++ ConnectionsService/src/main/java/com/ashish/linkedlnProject/ConnectionsService/exception/BadRequestException.java
@@ -0,0 +1,8 @@
+package com.ashish.linkedlnProject.ConnectionsService.exception;
+
+public class BadRequestException extends RuntimeException {
+    public BadRequestException(String message) {
+        super(message);
+    }
+
+}
---
+++ ConnectionsService/src/main/java/com/ashish/linkedlnProject/ConnectionsService/exception/ResourceNotFoundException.java
@@ -0,0 +1,7 @@
+package com.ashish.linkedlnProject.ConnectionsService.exception;
+
+public class ResourceNotFoundException extends RuntimeException {
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+}
